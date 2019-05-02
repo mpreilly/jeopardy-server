934c1335676eb3459443af1ae30337f92b213971
mpreilly <mpreilly@gatech.edu>
2019-05-02 14:57:30 -0400

    added /games/<date> endpoint for getting games

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ if (!fs.existsSync('gameObjs.json')) {
 var content = fs.readFileSync('gameObjs.json');
 var gameObjs = JSON.parse(content);
 var gameDates = Object.keys(gameObjs)
-console.log(gameObjs[gameDates[0]])
+// console.log(gameObjs[gameDates[0]])
 
-app.get('/', (req, res) => res.send('Hello, World!\n'))
+app.get('/', (req, res) => res.send('hit the /dates url to get dates. hit /games/<date> to get a game.\n'))
 
 app.get('/dates', (req, res) => res.send(JSON.stringify(gameDates)))
 
+app.get('/games/:date', (req, res) =>  {
+    res.send(gameObjs.hasOwnProperty(req.params.date) ?
+        gameObjs[req.params.date] : "Error: No full game for date " + req.params.date)
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
